cb1457755973a79b9ea556a3a61f142c99e89565
rohanpra <rohanpra@umich.edu>
2021-03-16 16:46:34 -0400

    my account page

--- src/pages/MyAccountPage/MyAccountPage.js
+++ src/pages/MyAccountPage/MyAccountPage.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./MyAccountPage.scss";
+import { useRouteMatch } from "react-router-dom";
+import { numberToClassName } from "./../../utils/ClassUtils";
 import NavBar from "../../components/NavBar/NavBar";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
@@ -13,191 +15,123 @@ import { allClasses } from "../../utils/ClassUtils";
 import Context from "../../contexts/context";
 import firebase from "firebase/app";
 import "firebase/database";
+import Modal from "react-modal";
 
-function AddReviewPage() {
-  const { user } = useContext(Context);
-  const options = [
-    "Winter 2021",
-    "Fall 2020",
-    "Winter 2020",
-    "Fall 2019",
-    "Winter 2019",
-    "Fall 2018",
-  ];
-  const [semester, setState] = useState(options[0]);
-  const [entries, setEntries] = useState([
-    {
-      class: "",
-      professor: "",
-      workload: 0,
-      quality: 0,
-      usefulness: 0,
-      comment: "",
-    },
-  ]);
-  const handleSubmitEntry = () => {
-    firebase
-      .database()
-      .ref("users/" + user.uid + "/reviews/" + semester)
-      .set(entries);
-    entries.forEach((entry) => {
-      console.log("h");
-      entry.authorName = user.displayName;
-      entry.authorID = user.uid;
-      entry.semester = semester;
+const customStyles = {
+  content: {
+    width: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+function MyAccountPage() {
+  const [reviews, setReviews] = useState([]);
+  const [fullReview, setFullReview] = useState({});
+  const { user, signInWithGoogle } = useContext(Context);
+  
+  useEffect(() => {
+    // console.log("ASDFASDFASDFASDFASDF");
+    // console.log(user);
+    if (user) {
       firebase
-        .database()
-        .ref("class/" + entry.class + "/")
-        .push(entry);
-    });
-  };
-  const handleAddEntry = () => {
-    setEntries([
-      ...entries,
-      {
-        class: "",
-        professor: "",
-        workload: 0,
-        quality: 0,
-        usefulness: 0,
-        comment: "",
-      },
-    ]);
-    setTimeout(() => {
-      window.scroll({
-        top: document.body.offsetHeight,
-        left: 0,
-        behavior: "smooth",
+      .database()
+      .ref("users/" + user.uid + "/reviews/")
+      .get()
+      .then(function (snapshot) {
+        if (snapshot.exists()) {
+          setReviews(snapshot.val());
+          console.log("snapshot exists");
+          console.log(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
       });
-    }, 0);
-  };
-  const handleRemoveEntry = () => {
-    setEntries(entries.slice(0, entries.length - 1));
-  };
-  function renderMenu(items, value, style) {
-    return value === "" ? (
-      <div></div>
-    ) : (
-      <div
-        style={{
-          ...style,
-          ...this.menuStyle,
-          maxHeight: "300px",
-          zIndex: 2,
-        }}
-        children={items}
-      />
-    );
-  }
+    }
+    
+  }, [user]);
+
   return (
-    <div>
+    <div className="classPage">
       <NavBar />
-      <div className="select-semester">
-        <p className="account-heading">Welcome James</p>
-      </div>
-      <div className="entryContainer">
-        {entries.map((entry, idx) => {
-          return (
-            <div className="entry" key={"entry" + idx}>
-              <div className="leftEntry">
-                <p>
-                  <input
-                    value={entry.professor}
-                    onChange={(e) => {
-                      entries[idx].professor = e.target.value;
-                      setEntries([...entries]);
-                    }}
-                    className="searchbar"
-                    placeholder={"Professor"}
-                  />
-                </p>
+      {true ? (
+        <div className="mainClassContainer">
+          <div className="reviewsContainer">
+            <h2>{reviews.length} Student Reviews</h2>
+            {Object.entries(reviews).map(([sem_key, review_sem]) => {
+              return (
                 <div>
-                  Workload
-                  <ReactStars
-                    count={5}
-                    value={entries[idx].workload}
-                    onChange={(rating) => {
-                      entries[idx].workload = rating;
-                      setEntries([...entries]);
-                    }}
-                    size={36}
-                    color2={"#ffd700"}
-                  />
+                  <div>Reviews for: {sem_key}</div>
+                  <br></br>
+                  <div>
+                  {review_sem["classes"].map((review, idx) => {
+              return (
+                <div className="review" key={"review" + idx}>
+                  {/* <div>Semester: {review[0].classes[0]}</div> */}
+                  <div className="leftReview">
+                    <p className="reviewAuthor">{review.class}</p>
+                    <p className="reviewInfo">
+                      Prof. {review.professor} ({sem_key})
+                    </p>
+                    <div className="reviewStarsContainer">
+                      <div className="starContainer">
+                        <h2>Workload</h2>
+                        <ReactStars
+                          edit={false}
+                          count={5}
+                          value={review.workload}
+                          size={28}
+                          color2={"#ffd700"}
+                        />
+                      </div>
+                      <div className="starContainer">
+                        <h2>Quality</h2>
+                        <ReactStars
+                          edit={false}
+                          count={5}
+                          value={review.quality}
+                          size={28}
+                          color2={"#ffd700"}
+                        />
+                      </div>
+                      <div className="starContainer">
+                        <h2>Usefulness</h2>
+                        <ReactStars
+                          edit={false}
+                          count={5}
+                          value={review.usefulness}
+                          size={28}
+                          color2={"#ffd700"}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="rightReview">
+                    <h2>Feedback:</h2>
+                    <p>{review.comment}</p>
+                  </div>
                 </div>
-                <div>
-                  Quality
-                  <ReactStars
-                    count={5}
-                    value={entries[idx].quality}
-                    onChange={(rating) => {
-                      entries[idx].quality = rating;
-                      setEntries([...entries]);
-                    }}
-                    size={36}
-                    color2={"#ffd700"}
-                  />
+              );
+            })}
+                  </div>
                 </div>
-                <div>
-                  Usefulness
-                  <ReactStars
-                    count={5}
-                    value={entries[idx].usefulness}
-                    onChange={(rating) => {
-                      entries[idx].usefulness = rating;
-                      setEntries([...entries]);
-                    }}
-                    size={36}
-                    color2={"#ffd700"}
-                  />
-                </div>
-              </div>
-              <textarea
-                value={entries[idx].comment}
-                onChange={(e) => {
-                  entries[idx].comment = e.target.value;
-                  setEntries([...entries]);
-                }}
-                className="mainInput"
-                placeholder="Enter a comment..."
-              />
-            </div>
-          );
-        })}
-      </div>
-      <div className="buttonContainer">
-        <a
-          onClick={handleAddEntry}
-          className="addButton"
-          href="#"
-          role="button"
-        >
-          <span>add</span>
-          <div className="icon">
-            <FaPlusCircle size={32} />
+              );
+            })}
+            
           </div>
-        </a>
-        {entries.length > 1 && (
-          <a onClick={handleRemoveEntry} className="removeButton" role="button">
-            <span>remove</span>
-            <div className="icon">
-              <FaMinusCircle size={32} />
-            </div>
-          </a>
-        )}
-        <a
-          onClick={handleSubmitEntry}
-          className="submitButton"
-          href="#"
-          role="button"
-        >
-          <span>Submit</span>
-          <div className="icon">
-            <FaCheckCircle size={32} />
-          </div>
-        </a>
-      </div>
+        </div>
+      ) : (
+        <div>No entries found</div>
+      )}
     </div>
   );
 }
 
-export default AddReviewPage;
+export default MyAccountPage;
