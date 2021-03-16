dc266d1274379959c491caafef7ac936e10e1aab
James Xu <jamesxu@umich.edu>
2021-03-15 22:15:31 -0400

    build for github pages

---
+++ build/precache-manifest.d18507d1bfe09d8869928a110fa9d470.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed75eef0152b006dcb38c9334a82950c",
+    "url": "/ReviewU/index.html"
+  },
+  {
+    "revision": "2dda0c12246c30f36367",
+    "url": "/ReviewU/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "1535590d0aa00c7a41f5",
+    "url": "/ReviewU/static/css/main.07f32647.chunk.css"
+  },
+  {
+    "revision": "2dda0c12246c30f36367",
+    "url": "/ReviewU/static/js/2.3e19bacf.chunk.js"
+  },
+  {
+    "revision": "952e0a46cdb1197e3ba0108f09e6b242",
+    "url": "/ReviewU/static/js/2.3e19bacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1535590d0aa00c7a41f5",
+    "url": "/ReviewU/static/js/main.9b4ff7e2.chunk.js"
+  },
+  {
+    "revision": "cd6952d453046f7f1137",
+    "url": "/ReviewU/static/js/runtime-main.4250b786.js"
+  }
+]);
