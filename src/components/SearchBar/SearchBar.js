ebb3d2581bb166cb2b5cdb2d4c5ac41494d0def4
James Xu <jamesxu@umich.edu>
2021-03-16 17:37:54 -0400

    changed searchbar zindex

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,67 +5,59 @@ import Autocomplete from "react-autocomplete";
 import { allClasses } from "./../../utils/ClassUtils";
 import { useHistory } from "react-router-dom";
 
-
 function SearchBar(props) {
+  const [value, setValue] = useState("");
+  const history = useHistory();
 
-    const [value, setValue] = useState("");
-    const history = useHistory();
-
-    function renderMenu(items, value, style) {
-        return value === "" ? (
-          <div></div>
-        ) : (
-          <div
-            style={{ ...style, ...this.menuStyle, maxHeight: "300px" }}
-            children={items}
-          />
-        );
-    }
-
-    return (
-        <Autocomplete
-          className="searchbar"
-          getItemValue={(item) => item}
-          items={allClasses}
-          renderItem={(item, isHighlighted) => (
-            <div
-              style={{
-                background: isHighlighted ? "lightgray" : "white",
-                height: "40px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                fontSize: "20px",
-              }}
-            >
-              {item}
-            </div>
-          )}
-          selectOnBlur={true}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          onSelect={(val) => {
-            history.push("/class/" + val);
-          }}
-          shouldItemRender={(item, value) =>
-            item.toLowerCase().startsWith(value.toLowerCase()) ||
-            item
-              .toLowerCase()
-              .replace("-", " ")
-              .startsWith(value.toLowerCase()) ||
-            item
-              .toLowerCase()
-              .replace("-", "")
-              .startsWith(value.toLowerCase()) ||
-            item.endsWith(value)
-          }
-          renderMenu={renderMenu}
-          inputProps={{
-            className: "searchbar",
-            placeholder: "Enter a class...",
-          }}
-        />
+  function renderMenu(items, value, style) {
+    return value === "" ? (
+      <div></div>
+    ) : (
+      <div
+        style={{ ...style, ...this.menuStyle, maxHeight: "300px", zIndex: 2 }}
+        children={items}
+      />
     );
+  }
+
+  return (
+    <Autocomplete
+      className="searchbar"
+      getItemValue={(item) => item}
+      items={allClasses}
+      renderItem={(item, isHighlighted) => (
+        <div
+          style={{
+            background: isHighlighted ? "lightgray" : "white",
+            height: "40px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "20px",
+          }}
+        >
+          {item}
+        </div>
+      )}
+      selectOnBlur={true}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      onSelect={(val) => {
+        history.push("/class/" + val);
+      }}
+      shouldItemRender={(item, value) =>
+        item.toLowerCase().startsWith(value.toLowerCase()) ||
+        item.toLowerCase().replace("-", " ").startsWith(value.toLowerCase()) ||
+        item.toLowerCase().replace("-", "").startsWith(value.toLowerCase()) ||
+        item.endsWith(value)
+      }
+      renderMenu={renderMenu}
+      inputProps={{
+        className: "searchbar",
+        placeholder: "Enter a class...",
+      }}
+    />
+  );
 }
 
 export default SearchBar;
