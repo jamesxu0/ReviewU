550cf22ef7cc4c1d71ad708eff9bd1a8e5cb8fad
James Xu <jamesxu@umich.edu>
2021-04-21 16:14:26 -0400

    Updates

---
+++ precache-manifest.2be0ebe0d26aba822fa87db298b5b672.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a373f1014c4c2a8f57f18a994cd58146",
+    "url": "/ReviewU/index.html"
+  },
+  {
+    "revision": "5812201522c348e6405e",
+    "url": "/ReviewU/static/css/2.ee241de9.chunk.css"
+  },
+  {
+    "revision": "5c13a02bd94a8e25f731",
+    "url": "/ReviewU/static/css/main.5ae19b61.chunk.css"
+  },
+  {
+    "revision": "5812201522c348e6405e",
+    "url": "/ReviewU/static/js/2.55b03b22.chunk.js"
+  },
+  {
+    "revision": "75c265e30c278bde45d32fc35e9d8394",
+    "url": "/ReviewU/static/js/2.55b03b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c13a02bd94a8e25f731",
+    "url": "/ReviewU/static/js/main.6b333aca.chunk.js"
+  },
+  {
+    "revision": "cd6952d453046f7f1137",
+    "url": "/ReviewU/static/js/runtime-main.4250b786.js"
+  }
+]);
